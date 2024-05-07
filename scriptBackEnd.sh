525aa868445c7a91605becbe6b8a6c49d21c9cfc
Isabela Saori <110927856+isabelasaorinakano@users.noreply.github.com>
2024-05-07 19:19:28 -0300

    alterando script BackEnd

--- scriptBackEnd.sh
+++ scriptBackEnd.sh
@@ -8,7 +8,7 @@ clear
 #Instalar o Java 
 echo "Instalando o Java..."
 sleep 0.5
-sudo apt install default-jdk -y
+sudo apt install oracle-java17-installer
 
 clear 
 #Instalar o Maven 
@@ -27,16 +27,20 @@ clear
 sudo usermod -a -G docker $(whoami)
 
 #renovando a sessão para aplicar as modificações ao usuário
-newgrp docker
+sudo newgrp docker
 
 #Criando o Container com o banco de dados pridepoints
 docker run --name bd_pride_ec2 -e MYSQL_ROOT_PASSWORD=966193319c -p 3306:3306 -d mysql:latest
+
+#Configurando variavel de ambiente
+export JAVA_HOME="/usr/lib/jvm/java-17-oracle/bin/java"
+
 
 #Clonando o projeto
 echo "Clonando o repositório do Back-End..."
 sleep 0.5
 
-git clone https://github.com/Pride-Points/Front-End.git
+git clone hhttps://github.com/Pride-Points/Back-End.git
 
 clear
 echo "Ambiente configurado com sucesso!"
